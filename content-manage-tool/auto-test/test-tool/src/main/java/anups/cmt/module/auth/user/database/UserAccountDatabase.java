f0289d8af94216a33ca1c3c2bf6923821fdc915b
N.L.N.Rao <nellutlalnrao@gmail.com>
2020-04-18 20:18:26 +0530

    Automated UserRegisterForm

--- content-manage-tool/auto-test/test-tool/src/main/java/anups/cmt/module/auth/user/database/UserAccountDatabase.java
+++ content-manage-tool/auto-test/test-tool/src/main/java/anups/cmt/module/auth/user/database/UserAccountDatabase.java
@@ -22,16 +22,12 @@ public class UserAccountDatabase {
 	   try{
 		 Class.forName(JDBC_DRIVER);
 		 conn = DriverManager.getConnection(DB_URL,USER,PASS);
-		 System.out.println("Connecting to database...");
-		 System.out.println("Creating statement...");
-	      stmt = conn.createStatement();
-	      String sql;
-	      sql = "SELECT * FROM user_accounts_sq ORDER BY sQ_Id;";
-	      rs = stmt.executeQuery(sql);
-	      while(rs.next()){
-	    	  sQ.add(rs.getString("sQ"));
-	      }
-	      
+		 stmt = conn.createStatement();
+	     String sql = "SELECT * FROM user_accounts_sq ORDER BY sQ_Id;";
+	     rs = stmt.executeQuery(sql);
+	     while(rs.next()){
+	       sQ.add(rs.getString("sQ"));
+	     } 
 		} catch(Exception e) {
 		    e.printStackTrace();
 	    } finally {	
@@ -50,16 +46,13 @@ public class UserAccountDatabase {
 		   try{
 			 Class.forName(JDBC_DRIVER);
 			 conn = DriverManager.getConnection(DB_URL,USER,PASS);
-			 System.out.println("Connecting to database...");
-			 System.out.println("Creating statement...");
-		      stmt = conn.createStatement();
-		      String sql1 = "SELECT count(*) FROM user_accounts_auth WHERE mobile='"+UserRegisterTestData.DATA_VALID_MOBILE_NEW+"';";
-		      rs = stmt.executeQuery(sql1);
-		      if(rs.next()){
-		    	  String sql2 = "DELETE FROM user_accounts_auth WHERE mobile='"+UserRegisterTestData.DATA_VALID_MOBILE_NEW+"';";
-		    	  stmt.executeUpdate(sql2);
-		      }
-		      
+			 stmt = conn.createStatement();
+		     String sql1 = "SELECT count(*) FROM user_accounts_auth WHERE mobile='"+UserRegisterTestData.DATA_VALID_MOBILE_NEW+"';";
+		     rs = stmt.executeQuery(sql1);
+		     if(rs.next()){
+		    	String sql2 = "DELETE FROM user_accounts_auth WHERE mobile='"+UserRegisterTestData.DATA_VALID_MOBILE_NEW+"';";
+		        stmt.executeUpdate(sql2);
+		     }
 			} catch(Exception e) {
 			    e.printStackTrace();
 		    } finally {	
